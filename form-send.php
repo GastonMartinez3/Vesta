c3e2cdf3dc9ce1ee43cde7b36809d5158c563bea
Gaston Martinez <gastonmartinezeq@gmail.com>
2022-08-22 20:44:02 -0300

    Ultima configuracion mail y php

--- form-send.php
+++ form-send.php
@@ -4,7 +4,7 @@ $nombre = $_POST['name'];
 $mail = $_POST['email'];
 $telefono = $_POST['phone'];
 $asunto = $_POST['subject'];
-$empresa = $_POST['menssage'];
+$msj = $_POST['menssage'];
 
 $header = 'From: ' . $mail . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
@@ -15,7 +15,7 @@ $mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
 $mensaje .= "Su e-mail es: " . $mail . " \r\n";
 $mensaje .= "Asunto: " . $asunto . " \r\n";
 $mensaje .= "Teléfono: " . $telefono . " \r\n";
-$mensaje .= "Mensaje: " . $empresa . " \r\n";
+$mensaje .= "Mensaje: " . $msj . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
 $para = 'paugarcia@bevesta.com.ar';
@@ -25,3 +25,5 @@ if (mail($para, $asunto, utf8_decode($mensaje), $header))
 echo "<script type='text/javascript'>window.location.href='http://bevesta.com.ar/form-confirm.html';</script>";
 
 ?>
+
+<!-- TENES POTENCIAL FLAQUITO !!  -->
